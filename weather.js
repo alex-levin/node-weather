444704f326880d51d915cd0f76008525931b9046
Alex Levin <alexlevinb@gmail.com>
2018-01-07 09:04:19 -0500

    weather.js

--- weather.js
+++ weather.js
@@ -3,12 +3,10 @@
 const http = require('http');
 const request = require('request');
 
-function init() {
+function init(url) {
     // Request URL and headers
     var options = {
-        // url: 'https://api.weather.gov/points/42.2856,-71.3851',
-        url: 'https://api.weather.gov/gridpoints/BOX/60,71/forecast',
-        // url: 'https://api.weather.gov/gridpoints/BOX/60,71/forecast/hourly',
+        url: url,
         headers: {
             'User-Agent': 'request',
             'accept': 'application/json'
@@ -27,12 +25,15 @@ function init() {
 }
 
 http.createServer(function (req, res) {
-   var promise = init();
+    // 'https://api.weather.gov/points/42.2856,-71.3851'
+    // 'https://api.weather.gov/gridpoints/BOX/60,71/forecast'
+    // 'https://api.weather.gov/gridpoints/BOX/60,71/forecast/hourly'
+   var promise = init('https://api.weather.gov/points/42.2856,-71.3851');
    promise.then(function(result) {
        console.log(result);
        res.writeHead(200, { 'Content-Type': 'text/plain' });
        res.write(
-          JSON.stringify(result, true, 2));
+          JSON.stringify(result, true, 4));
        res.end();
    }, function(err) {
        console.log(err);
